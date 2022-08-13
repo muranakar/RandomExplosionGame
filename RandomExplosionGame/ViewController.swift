ec5d2e167d67379eec750dd7746ed50156682b1f
muranakar <p1211065@gmail.com>
2022-08-13 22:35:24 +0900

    first commit

---
+++ RandomExplosionGame/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RandomExplosionGame
+//
+//  Created by 村中令 on 2022/08/13.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
